651e2970f4717a60a87dbd9d80b3eb156208fbfb
wjeliot <admin@williameliot.com>
2021-05-09 23:18:00 +0100

    feat: add keydown listener

--- pages/album/[name]/tag.js
+++ pages/album/[name]/tag.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import { useEffect, useState } from 'react'
 
 const fetcher = async (...args) => {
     const res = await fetch(...args)
@@ -10,16 +11,29 @@ export default function Tag() {
     const router = useRouter()
     const { name } = router.query
     const { data } = useSWR(`/api/album/${name}`, fetcher)
+    const [ currentImage, setCurrentImage ] = useState()
 
-    let urlList = data && Object.values(data).map(image => image.url).sort(() => Math.random() - 0.5)
+    useEffect(() => {
+        const handleKeyPress = (e) => {
+            if (e.keyCode === 37) {
+                console.log("you pressed left key")
+            }
+            if (e.keyCode === 39) {
+                console.log("you pressed right key")
+            }
+        }
+    
+        document.addEventListener('keydown', handleKeyPress)
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress)
+        }
+    }, [])
 
     return (
         <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
             <div>
-                <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-                    <svg class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-                    </svg>
+                <div>
+                    <img src={currentImage} />
                 </div>
                 <div class="mt-3 text-center sm:mt-5">
                     <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
@@ -38,6 +52,9 @@ export default function Tag() {
                 </button>
                 <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm">
                     Cancel
+                </button>
+                <button onClick={() => setCurrentImage(urlList[0])} type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm">
+                    Begin
                 </button>
             </div>
         </div>
